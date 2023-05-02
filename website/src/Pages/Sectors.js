f8653d6a96f5413e27924be76ccc5219065da96d
afeef raza <afeefraza691@gmail.com>
2023-05-02 17:13:09 +0500

    changed images
    changed images

--- website/src/Pages/Sectors.js
+++ website/src/Pages/Sectors.js
@@ -3,13 +3,13 @@ import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 
 //Sector images
-import image1 from "../assets/sectorsimg/1.jpeg";
-import image2 from "../assets/sectorsimg/2.jpeg";
-import image3 from "../assets/sectorsimg/3.jpeg";
-import image4 from "../assets/sectorsimg/4.jpeg";
-import image5 from "../assets/sectorsimg/5.jpeg";
-import image6 from "../assets/sectorsimg/6.jpeg";
-import image7 from "../assets/sectorsimg/7.jpeg";
+import image1 from "../assets/sectorsimg/oilgas.jpg";
+import image2 from "../assets/sectorsimg/power1.jpg";
+import image3 from "../assets/sectorsimg/refining.jpg";
+import image4 from "../assets/sectorsimg/petrochemical.jpg";
+import image5 from "../assets/sectorsimg/marine1.jpg";
+import image6 from "../assets/sectorsimg/industries.jpg";
+import image7 from "../assets/sectorsimg/infrastructure.jpg";
 
 import oilGas from "../assets/oilgasout.jpg";
 
@@ -28,10 +28,10 @@ function Sectors() {
                 style={{ maxHeight: "100%" }}
               >
                 <div class="portfolio-content h-100">
-                  <img src={image1} class="img-fluid sectorimg" alt="" />
+                  <img src={image1} class="img-fluid sectorimg" alt=""  style={{ height: "30vh" }}   />
                   <div class="portfolio-info">
                     <p>Oil & Gas</p>
-                    <a href={image1} title="Remodeling 1" class="glightbox preview-link">
+                    <a href={image1} title="Remodeling 1" class="glightbox preview-link"  style={{ height: "30vh" }}  >
                       <i class="bi bi-zoom-in"></i>
                     </a>
                     <a href={"/Sectors/OilGas"} title="More Details" class="details-link">
@@ -43,10 +43,10 @@ function Sectors() {
 
               <div class="col-lg-4 col-md-6 portfolio-item filter-construction">
                 <div class="portfolio-content h-100">
-                  <img src={image2} class="img-fluid sectorimg" alt="" />
+                  <img src={image2} class="img-fluid sectorimg" alt=""  style={{ height: "30vh" }}   />
                   <div class="portfolio-info">
                     <p>Power</p>
-                    <a href={image2} title="Construction 1" class="glightbox preview-link">
+                    <a href={image2} title="Construction 1" class="glightbox preview-link"  style={{ height: "30vh" }}  >
                       <i class="bi bi-zoom-in"></i>
                     </a>
 
@@ -59,10 +59,10 @@ function Sectors() {
 
               <div class="col-lg-4 col-md-6 portfolio-item filter-repairs">
                 <div class="portfolio-content h-100">
-                  <img src={image3} class="img-fluid sectorimg" alt="" />
+                  <img src={image3} class="img-fluid sectorimg" alt=""  style={{ height: "30vh" }}   />
                   <div class="portfolio-info">
                     <p>Refining</p>
-                    <a href={image3} title="Repairs 1" class="glightbox preview-link">
+                    <a href={image3} title="Repairs 1" class="glightbox preview-link"  style={{ height: "30vh" }}  >
                       <i class="bi bi-zoom-in"></i>
                     </a>
 
@@ -75,10 +75,10 @@ function Sectors() {
 
               <div class="col-lg-4 col-md-6 portfolio-item filter-design">
                 <div class="portfolio-content h-100">
-                  <img src={image4} class="img-fluid sectorimg" alt="" />
+                  <img src={image4} class="img-fluid sectorimg" alt=""  style={{ height: "30vh" }}   />
                   <div class="portfolio-info">
                     <p>Petrochemicals</p>
-                    <a href={image4} title="Repairs 1" class="glightbox preview-link">
+                    <a href={image4} title="Repairs 1" class="glightbox preview-link"  style={{ height: "30vh" }}  >
                       <i class="bi bi-zoom-in"></i>
                     </a>
                     <a href={"/Sectors/Petrochemical"} title="More Details" class="details-link">
@@ -90,10 +90,10 @@ function Sectors() {
 
               <div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
                 <div class="portfolio-content h-100">
-                  <img src={image5} class="img-fluid sectorimg" alt="" />
+                  <img src={image5} class="img-fluid sectorimg" alt=""  style={{ height: "30vh" }}   />
                   <div class="portfolio-info">
                     <p>Marine</p>
-                    <a href={image5} title="Remodeling 2" class="glightbox preview-link">
+                    <a href={image5} title="Remodeling 2" class="glightbox preview-link"  style={{ height: "30vh" }}  >
                       <i class="bi bi-zoom-in"></i>
                     </a>
                     <a href={"/Sectors/Marine"} title="More Details" class="details-link">
@@ -105,10 +105,10 @@ function Sectors() {
 
               <div class="col-lg-4 col-md-6 portfolio-item filter-construction">
                 <div class="portfolio-content h-100">
-                  <img src={image6} class="img-fluid sectorimg" alt="" />
+                  <img src={image6} class="img-fluid sectorimg" alt=""  style={{ height: "30vh" }}   />
                   <div class="portfolio-info">
                     <p>Major & Small Industries</p>
-                    <a href={image6} title="Construction 2" class="glightbox preview-link">
+                    <a href={image6} title="Construction 2" class="glightbox preview-link"  style={{ height: "30vh" }}  >
                       <i class="bi bi-zoom-in"></i>
                     </a>
                     <a href={"/Sectors/Industries"} title="More Details" class="details-link">
@@ -120,10 +120,10 @@ function Sectors() {
 
               <div class="col-lg-4 col-md-6 portfolio-item filter-repairs">
                 <div class="portfolio-content h-100">
-                  <img src={image7} class="img-fluid sectorimg" alt="" />
+                  <img src={image7} class="img-fluid sectorimg" alt=""  style={{ height: "30vh" }}   />
                   <div class="portfolio-info">
                     <p>Utility & Infrastructure</p>
-                    <a href={image7} title="Repairs 2" class="glightbox preview-link">
+                    <a href={image7} title="Repairs 2" class="glightbox preview-link"  style={{ height: "30vh" }}  >
                       <i class="bi bi-zoom-in"></i>
                     </a>
                     <a href={"/Sectors/Infrastructure"} title="More Details" class="details-link">
