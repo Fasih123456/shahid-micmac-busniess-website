2d023a77f718d1d9186aab34eb742dfeb36a3378
Fasih <fasihislam1999@gmail.com>
2023-11-16 19:17:23 -0500

    update changes with client feedback

--- website/src/Pages/Contact.js
+++ website/src/Pages/Contact.js
@@ -17,121 +17,123 @@ import "../Personalassets/Contact.css";
 import heading from "../assets/contactimg/heading.jpg";
 
 function Contact() {
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627,
-    },
-    zoom: 11,
-  };
+	return (
+		<React.Fragment>
+			<Header display={true} Route={"Contact"} image={heading} />
 
-  const AnyReactComponent = ({ text }) => <div>{text}</div>;
+			<section id="contact" class="contact">
+				<div class="container">
+					<div class="row gy-4">
+						<div class="col-lg-6">
+							<div class="info-item d-flex flex-column justify-content-center align-items-center">
+								<i class="bi bi-map"></i>
+								<h3>Our Address</h3>
+								<p>
+									Delma Street , Sultan Manea Al Muhairi
+									Building , Mez. Floor . P.O Box: 111303
+								</p>
+							</div>
+						</div>
 
-  return (
-    <React.Fragment>
-      <Header display={true} Route={"Contact"} image={heading} />
+						<div class="col-lg-3 col-md-6">
+							<div class="info-item d-flex flex-column justify-content-center align-items-center">
+								<i class="bi bi-envelope"></i>
+								<h3>Email Us</h3>
+								<p>info@midmac.ae</p>
+							</div>
+						</div>
 
-      <section id="contact" class="contact">
-        <div class="container">
-          <div class="row gy-4">
-            <div class="col-lg-6">
-              <div class="info-item d-flex flex-column justify-content-center align-items-center">
-                <i class="bi bi-map"></i>
-                <h3>Our Address</h3>
-                <p>
-                  Delma Street , Sultan Manea Al Muhairi Building , Mez. Floor . P.O Box: 111303
-                </p>
-              </div>
-            </div>
+						<div class="col-lg-3 col-md-6">
+							<div class="info-item d-flex flex-column justify-content-center align-items-center">
+								<i class="bi bi-telephone"></i>
+								<h3>Call Us</h3>
+								<p>+971 (0)2 667 1717</p>
+								<p>+971 56 302 1903</p>
+							</div>
+						</div>
+					</div>
 
-            <div class="col-lg-3 col-md-6">
-              <div class="info-item d-flex flex-column justify-content-center align-items-center">
-                <i class="bi bi-envelope"></i>
-                <h3>Email Us</h3>
-                <p>info@midmac.ae</p>
-              </div>
-            </div>
+					<div class="row gy-4 mt-1">
+						<div class="col-lg-6">
+							<iframe
+								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3631.599509595937!2d54.378323314978836!3d24.464676467030824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5e67c467b84e65%3A0xbb66aefd9b782e8d!2sMidMac%20General%20Trading%20LLC!5e0!3m2!1sen!2sca!4v1678304650161!5m2!1sen!2sca"
+								frameborder="0"
+								style={{
+									border: "0",
+									width: "100%",
+									height: "384px",
+								}}
+								allowfullscreen
+							></iframe>
+						</div>
 
-            <div class="col-lg-3 col-md-6">
-              <div class="info-item d-flex flex-column justify-content-center align-items-center">
-                <i class="bi bi-telephone"></i>
-                <h3>Call Us</h3>
-                <p>+971 (0)2 667 1717</p>
-                <p>+971 156 302 1903</p>
-              </div>
-            </div>
-          </div>
+						<div class="col-lg-6">
+							<form
+								action="forms/contact.php"
+								method="post"
+								role="form"
+								class="php-email-form"
+							>
+								<div class="row gy-4">
+									<div class="col-lg-6 form-group">
+										<input
+											type="text"
+											name="name"
+											class="form-control"
+											id="name"
+											placeholder="Your Name"
+											required
+										/>
+									</div>
+									<div class="col-lg-6 form-group">
+										<input
+											type="email"
+											class="form-control"
+											name="email"
+											id="email"
+											placeholder="Your Email"
+											required
+										/>
+									</div>
+								</div>
+								<div class="form-group">
+									<input
+										type="text"
+										class="form-control"
+										name="subject"
+										id="subject"
+										placeholder="Subject"
+										required
+									/>
+								</div>
+								<div class="form-group">
+									<textarea
+										class="form-control"
+										name="message"
+										rows="5"
+										placeholder="Message"
+										required
+									></textarea>
+								</div>
+								<div class="my-3">
+									<div class="loading">Loading</div>
+									<div class="error-message"></div>
+									<div class="sent-message">
+										Your message has been sent. Thank you!
+									</div>
+								</div>
+								<div class="text-center">
+									<button type="submit">Send Message</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</section>
 
-          <div class="row gy-4 mt-1">
-            <div class="col-lg-6">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3631.599509595937!2d54.378323314978836!3d24.464676467030824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5e67c467b84e65%3A0xbb66aefd9b782e8d!2sMidMac%20General%20Trading%20LLC!5e0!3m2!1sen!2sca!4v1678304650161!5m2!1sen!2sca"
-                frameborder="0"
-                style={{ border: "0", width: "100%", height: "384px" }}
-                allowfullscreen
-              ></iframe>
-            </div>
-
-            <div class="col-lg-6">
-              <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-                <div class="row gy-4">
-                  <div class="col-lg-6 form-group">
-                    <input
-                      type="text"
-                      name="name"
-                      class="form-control"
-                      id="name"
-                      placeholder="Your Name"
-                      required
-                    />
-                  </div>
-                  <div class="col-lg-6 form-group">
-                    <input
-                      type="email"
-                      class="form-control"
-                      name="email"
-                      id="email"
-                      placeholder="Your Email"
-                      required
-                    />
-                  </div>
-                </div>
-                <div class="form-group">
-                  <input
-                    type="text"
-                    class="form-control"
-                    name="subject"
-                    id="subject"
-                    placeholder="Subject"
-                    required
-                  />
-                </div>
-                <div class="form-group">
-                  <textarea
-                    class="form-control"
-                    name="message"
-                    rows="5"
-                    placeholder="Message"
-                    required
-                  ></textarea>
-                </div>
-                <div class="my-3">
-                  <div class="loading">Loading</div>
-                  <div class="error-message"></div>
-                  <div class="sent-message">Your message has been sent. Thank you!</div>
-                </div>
-                <div class="text-center">
-                  <button type="submit">Send Message</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <Footer />
-    </React.Fragment>
-  );
+			<Footer />
+		</React.Fragment>
+	);
 }
 
 export default Contact;
--- website/src/Pages/Products.js
+++ website/src/Pages/Products.js
@@ -2,57 +2,75 @@ import React from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 
+import img from "../assets/products-page-img.jpg";
+
 import "../App.css";
 
 function Products() {
-  return (
-    <React.Fragment>
-      <Header display={true} Route={"Scope Of Products"} />
-      <main id="main">
-        <section style={{ marginLeft: "5%" }}>
-          <h1
-            style={{
-              color: "var(--color-primary)",
-              fontSize: "2rem",
-              fontFamily: "var(--font-secondary)",
-            }}
-          >
-            Scope of supply:
-          </h1>
-          <ul style={{ fontSize: "1.2rem", fontFamily: "var(--font-secondary)" }}>
-            <li> Choke Valve</li>
-            <li>Double block and bleed valve</li>
-            <li> Control Valves-All type</li>
-            <li> MOVS,ESD</li>
-            <li> Manual Valves (Gate, Globe Check & Butterfly Valve)</li>
-            <li> Field Instruments (Gauges & Transmitters)</li>
-            <li> Cables-Electrical & Instruments</li>
-            <li> EXd Enclosures & Junction Box</li>
-            <li> Explosion Proof Lights</li>
-            <li> Sewage Treatment Packages</li>
-            <li> HVAC & Industrial Valve</li>
-          </ul>
-          <h1
-            style={{
-              color: "var(--color-primary)",
-              fontSize: "2rem",
-              fontFamily: "var(--font-secondary)",
-            }}
-          >
-            Scope of supply & installation:
-          </h1>
-          <ul style={{ fontSize: "1.2rem", fontFamily: "var(--font-secondary)" }}>
-            <li> Mechanical Key Interlocks</li>
-            <li> Passive Fire Protection</li>
-            <li> Actuators- Electric, Pneumatic & Electrohydraulic</li>
-            <li> Telecom & F&G (CCTV)</li>
-            <li> HVAC System</li>
-          </ul>
-        </section>
-      </main>
-      <Footer />
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Header display={true} Route={"Scope Of Products"} image={img} />
+			<main id="main">
+				<section style={{ marginLeft: "5%" }}>
+					<h1
+						style={{
+							color: "var(--color-primary)",
+							fontSize: "2rem",
+							fontFamily: "var(--font-secondary)",
+						}}
+					>
+						Scope of supply:
+					</h1>
+					<ul
+						style={{
+							fontSize: "1.2rem",
+							fontFamily: "var(--font-secondary)",
+						}}
+					>
+						<li> Choke Valve</li>
+						<li>Double block and bleed valve</li>
+						<li> Control Valves-All type</li>
+						<li> MOVS,ESD</li>
+						<li>
+							{" "}
+							Manual Valves (Gate, Globe Check & Butterfly Valve)
+						</li>
+						<li> Field Instruments (Gauges & Transmitters)</li>
+						<li> Cables-Electrical & Instruments</li>
+						<li> EXd Enclosures & Junction Box</li>
+						<li> Explosion Proof Lights</li>
+						<li> Sewage Treatment Packages</li>
+						<li> HVAC & Industrial Valve</li>
+					</ul>
+					<h1
+						style={{
+							color: "var(--color-primary)",
+							fontSize: "2rem",
+							fontFamily: "var(--font-secondary)",
+						}}
+					>
+						Scope of supply & installation:
+					</h1>
+					<ul
+						style={{
+							fontSize: "1.2rem",
+							fontFamily: "var(--font-secondary)",
+						}}
+					>
+						<li> Mechanical Key Interlocks</li>
+						<li> Passive Fire Protection</li>
+						<li>
+							{" "}
+							Actuators- Electric, Pneumatic & Electrohydraulic
+						</li>
+						<li> Telecom & F&G (CCTV)</li>
+						<li> HVAC System</li>
+					</ul>
+				</section>
+			</main>
+			<Footer />
+		</React.Fragment>
+	);
 }
 
 export default Products;
